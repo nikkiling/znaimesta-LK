f9e0e9b0efe435ca82335e1b2d4760637eb00e81
sburdeynaya <svetlana.b1977@gmail.com>
2019-08-21 15:29:40 +0300

    save

--- js/script.js
+++ js/script.js
@@ -10,3 +10,51 @@ $(document).ready(function() {
     });
 
 });
+
+//accordion
+
+$(document).ready(function() {
+
+    $('.accordion').on('click', '.accordion-control', function(e) {
+        e.preventDefault();
+        $('.accordion-panel').not(':animated').slideToggle();
+    });
+});
+
+//tabs
+$(document).ready(function(){
+
+    $('.tab-link').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('.tab-link').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
+
+});
+
+$(document).ready(function() {
+    $(".js-add-bookmark").click(function() {
+        $(this).toggleClass("chosen");
+    });
+});
+
+$(document).ready(function() {
+
+    $('.js-slider').on('initialized.owl.carousel changed.owl.carousel', function(e) {
+        if (!e.namespace)  {
+            return;
+        }
+        var carousel = e.relatedTarget;
+        $('.slider-counter').text(carousel.relative(carousel.current()) + 1 + ' из ' + carousel.items().length);
+    }).owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        loop:true,
+        margin:10
+    });
+});
